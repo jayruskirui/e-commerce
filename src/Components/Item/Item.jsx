d0635270eb2aa871952b6227370831be3eeda6df
JAYRUS KIPROTICH <jayruskirui@gmail.com>
2026-03-17 00:31:21 +0300

    Case debug on Item.jsx

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import './Item.css'
 import { Link } from 'react-router-dom'
+import  {useContext} from 'react'
+import { ShopContext } from '../../Context/ShopContext' 
 
 const Item = (props) => {
+  const { addToCart } = useContext(ShopContext)
   return (
     <div className="item">
-        <Link to={`/product/${props.id}`}><img onClick={window.scrollTo(0,0)}src={props.image} alt={props.name} /></Link>
-        {/* <img src={props.image} alt={props.name}/> */}
+        {/* <Link to={`/product/${props.id}`}><img onClick={window.scrollTo(0,0)}src={props.image} alt={props.name} /></Link> */}
+        <Link to='/cart'><img onClick={()=>{addToCart(props.id);window.scrollTo(0,0);}} src={props.image} alt={props.name} /></Link>
+        <img src={props.image} alt={props.name}/>
         <p>{props.name}</p>
         <div className="item-prices">
             <div className="item-price-new">
