4bd5f1b2a0a9742283620aef5363815526a16ed5
JAYRUS KIPROTICH <jayruskirui@gmail.com>
2026-06-14 01:08:13 +0300

    Enviroment variable and connection to mongodb

---
+++ Backend/index.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first');
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
+const port = process.env.PORT || 4000;
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+// Database connection
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB connected successfully"))
+.catch((err) => console.log("MongoDB connection error:", err))
+
+// API Creation
+app.get("/", (req, res) => {
+    res.send("Express App is running")
+})
+
+app.listen(port, (error) => {
+    if (!error) {
+        console.log("Server running on Port " + port)
+    } else {
+        console.log("Error: " + error)
+    }
+})
