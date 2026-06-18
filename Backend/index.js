9ce1d83e90f67bf5cd7cf31d2dcc3098ab0155ca
JAYRUS KIPROTICH <jayruskirui@gmail.com>
2026-06-18 14:10:02 +0300

    index.js updates

--- Backend/index.js
+++ Backend/index.js
@@ -12,6 +12,7 @@ const jwt = require("jsonwebtoken");
 const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
+const { log } = require("console");
 
 app.use(express.json());
 app.use(cors());
@@ -52,7 +53,7 @@ app.post('/upload', upload.single('product'), (req,res)=>{
 
 // schema for creating products
 
-const product = mongoose.model ("product",{
+const Product = mongoose.model ("product",{
     id:{
         type:Number,
         required:true,
@@ -86,6 +87,59 @@ const product = mongoose.model ("product",{
         default:true,
     }
 })
+
+app.post('/addproduct', async (req,res)=>{
+    let products = await Product.find({});
+    let id;
+    if(products.length>0)
+    {
+        let last_product_array = products.slice(-1);
+        let last_product = last_product_array[0];
+        id = last_product.id+1;
+    }
+    else{
+        id=1;   
+    }
+    const product = new Product ({
+        id: id,
+        name: req.body.name,
+        image: req.body.image,
+        category: req.body.category,
+        new_price: req.body.new_price,
+        old_price: req.body.old_price,
+    });
+    console.log (product);
+    await product.save();
+    console.log ("Saved");
+    res.json({
+        success:true,
+        name:req.body.name,
+    })
+});
+
+
+
+// Creating API for deleting products
+
+app.post ('/removeproduct', async (req,res)=>{
+    await Product.findOneAndDelete({id:req.body.id});
+    console.log('Removed')
+    res.json({
+        success: true,
+        name:req.body.name
+    })
+})
+
+
+
+// Creating API for getting all products
+
+app.get ('/allproducts',async (req,res)=>{
+    let products = await Product.find({});
+    console.log("All Products Fetched")
+    res.send(products);
+})
+
 
 
 app.listen(port, (error) => {
