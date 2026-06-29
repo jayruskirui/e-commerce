07b0a46975894318bde77117e7dd1c72e3f4cb05
JAYRUS KIPROTICH <jayruskirui@gmail.com>
2026-06-29 19:43:12 +0300

    Uploading of products to database

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -16,6 +16,18 @@ const ListProduct = () => {
     fetchInfo();
   },[])
 
+  const remove_product = async (id) => {
+    await fetch ('http://localhost:4000/removeproduct',{
+      method: 'POST',
+      headers:{
+        Accept:'application/json',
+        'content-type':'application/json',
+      },
+      body:JSON.stringify({id:id})
+    })
+    await fetchInfo();
+  }
+
 
   return (
     <div className='list-product'>
@@ -38,7 +50,7 @@ const ListProduct = () => {
             <p>${product.new_price}</p>
             <p>${product.old_price}</p>
             <p>{product.category}</p>
-            <img src={cross_icon} alt='' className='listproduct-remove-icon' />
+            <img onClick={()=>{remove_product(product.id)}} src={cross_icon} alt='' className='listproduct-remove-icon' />
           </div>
           <hr/>
           </>
