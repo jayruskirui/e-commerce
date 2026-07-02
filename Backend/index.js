33e0ab59a19c16c03c82c8b471d4f0f71be356db
JAYRUS KIPROTICH <jayruskirui@gmail.com>
2026-07-02 21:19:31 +0300

    creating endpoint for newcollection data

--- Backend/index.js
+++ Backend/index.js
@@ -216,6 +216,15 @@ app.post('/login', async (req,res) =>{
 })
 
 
+// creating endpoint for newcollection data
+app.get('/newcollection', async (req,res) => {
+    let products = await Product.find({});
+    let newcollection = products.slice(1).slice(-8);
+    console.log("New Collection Fetched");
+    res.send(newcollection);
+})
+
+
 app.listen(port, (error) => {
     if (!error) {
         console.log("Server running on Port " + port)
