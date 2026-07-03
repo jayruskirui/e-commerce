e60da3aa8c311d71991ceda82e6bf0c4782fb8b9
JAYRUS KIPROTICH <jayruskirui@gmail.com>
2026-07-03 18:01:29 +0300

    Creating endpoint for popular in women section

--- Backend/index.js
+++ Backend/index.js
@@ -225,6 +225,15 @@ app.get('/newcollection', async (req,res) => {
 })
 
 
+// Creating endpoint for popular in women section
+app.get('/popularinwomen', async (req,res) => {
+    let products = await Product.find({category:"women"});
+    let popular_in_women = products.slice(0,4);
+    console.log("Popular in Women Fetched");
+    res.send(popular_in_women);
+})
+
+
 app.listen(port, (error) => {
     if (!error) {
         console.log("Server running on Port " + port)
